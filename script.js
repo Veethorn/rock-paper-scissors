6e6b2963c6cafeb8f1e28d21e9f307d18ce71464
David Santos <dfps.gestao@gmail.com>
2022-11-04 07:19:32 +0000

    update script.js to return game result

--- script.js
+++ script.js
@@ -11,22 +11,36 @@ function getComputerChoice() {
 // write a function that plays a single round of rock paper scissors.
 // The function should return a message 
 
-function test () {
-    let user = prompt ("Your play", "");
-    let userChoice = user 
-    return (userChoice);
-   
+function playRound(playerSelection, computerSelection) {
+      
+      if (playerSelection=="rock" && computerSelection=="paper")
+      return alert("You went with rock and the computer with paper: Bad luck, you lost...")
+      else if (playerSelection=="rock" && computerSelection=="rock")
+      return alert("Humm, you both went with rock: It's a tie.")
+      else if (playerSelection=="rock" && computerSelection=="scissors")
+      return alert("You went with rock and the computer with scissors: Congrats, you won!"); 
+
+      if (playerSelection=="paper" && computerSelection=="scissors")
+      return alert("You went with paper and the computer with scissors: Bad luck, you lost...")
+      else if (playerSelection=="paper" && computerSelection=="paper")
+      return alert("Humm, you both went with paper: It's a tie.")
+      else if (playerSelection=="paper" && computerSelection=="rock")
+      return alert("You went with paper and the computer with rock: Congrats, you won!"); 
+
+      if (playerSelection=="scissors" && computerSelection=="rock")
+      return alert("You went with scissors and the computer with rock: Bad luck, you lost...")
+      else if (playerSelection=="scissors" && computerSelection=="scissors")
+      return alert("Humm, you both went with scissors: It's a tie.")
+      else if (playerSelection=="scissors" && computerSelection=="paper")
+      return alert("You went with scissors and the computer with paper: Congrats, you won!"); 
+      else return alert("Humm, I don't recognize that weapon... Please try again!")
 }
 
-function versus () {
-    if (test () == "rock") return getComputerChoice (); 
-    else return "testing";
-}
+let playerInput = prompt("Choose your weapon: rock, paper or scissors!").toUpperCase();
+const playerSelection = playerInput.toLowerCase(); 
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 
-function versus2 () {
-    let computerChoice = getComputerChoice()
-    console.log (computerChoice);
-    if (computerChoice == "rock") return "rock!"; 
-    else if (computerChoice == "paper") return "paper!";
-    else return "scissors!!";
-}
+    
+
+
